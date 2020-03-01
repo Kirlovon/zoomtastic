c5addd2faab938bbe3395d3a4c3764f1a9f87fc3
Kirill <Kirlovon@outlook.com>
2020-03-01 23:06:15 +0200

    Update

--- assets/code.js
+++ assets/code.js
@@ -1,1 +1,8 @@
 new Zoomtastic().listen('zoomtastic');
+
+// Preload full-sized images
+var images = ['image1.png', 'image2.png', 'image3.jpg'];
+for (var i = 0; i < images.length; i++) {
+	var image = new Image();
+	image.src = './assets/' + images[i];
+}
