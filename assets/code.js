e6b9647484bc56d20834ce4461194c1cb491db7e
Kirlovon <Kirlovon@outlook.com>
2022-04-28 01:38:36 +0200

    Added example to the project's website

--- assets/code.js
+++ assets/code.js
@@ -1,4 +1,6 @@
-Zoomtastic.mount({ animation: 'slide' });
+const DURATION = 300;
+
+Zoomtastic.mount({ animation: 'slide', duration: DURATION });
 Zoomtastic.listen('[zoomtastic]', 'zoomtastic');
 
 var slideButton = document.getElementById('slide');
@@ -9,25 +11,25 @@ var dropButton = document.getElementById('drop');
 slideButton.addEventListener('click', () => {
 	unclickButtons();
 	slideButton.classList.add('active');
-	Zoomtastic.mount({ animation: 'slide' });
+	Zoomtastic.mount({ animation: 'slide', duration: DURATION });
 });
 
 fadeButton.addEventListener('click', () => {
 	unclickButtons();
 	fadeButton.classList.add('active');
-	Zoomtastic.mount({ animation: 'fade' });
+	Zoomtastic.mount({ animation: 'fade', duration: DURATION });
 });
 
 zoomButton.addEventListener('click', () => {
 	unclickButtons();
 	zoomButton.classList.add('active');
-	Zoomtastic.mount({ animation: 'zoom' });
+	Zoomtastic.mount({ animation: 'zoom', duration: DURATION });
 });
 
 dropButton.addEventListener('click', () => {
 	unclickButtons();
 	dropButton.classList.add('active');
-	Zoomtastic.mount({ animation: 'drop' });
+	Zoomtastic.mount({ animation: 'drop', duration: DURATION });
 });
 
 function unclickButtons() {
@@ -37,9 +39,37 @@ function unclickButtons() {
 	dropButton.classList.remove('active');
 }
 
-// Preload full-sized images
-var images = ['image1.png', 'image2.png', 'image3.jpg'];
-for (var i = 0; i < images.length; i++) {
-	var image = new Image();
-	image.src = './assets/' + images[i];
-}
+const codeExample = `
+<!-- Add Zoomtastic attribute to zoomable elements! -->
+<img zoomtastic src="https://via.placeholder.com/256" />
+
+<!-- Load Zoomtastic from the CDN! -->
+<script src="https://unpkg.com/zoomtastic@2.1.0"></script>
+
+<script>
+
+	// Mount viewer elements
+	Zoomtastic.mount({
+		size: '95%',
+		easing: 'ease',
+		duration: 500,
+		background: 'rgba(0, 0, 0, 0.9)',
+		filter: 'drop-shadow(0 2px 16px rgba(0, 0, 0, 0.3))',
+		animation: 'slide' // Can be slide, fade, zoom or drop
+	});
+
+	// Show image manually
+	Zoomtastic.show('https://via.placeholder.com/256');
+
+	// Hide image
+	Zoomtastic.hide();
+
+	// Listen for an elements that contains "zoomtastic" attribute, 
+	// and use "src" attribute as image source
+	Zoomtastic.listen('[zoomtastic]', 'src');
+
+</script>
+`;
+
+const renderedExample = hljs.highlight(codeExample.trim(), { language: 'xml' }).value
+document.getElementById('code-example').innerHTML = renderedExample;
